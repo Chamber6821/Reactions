bc2806be7c55035af705c0aa556fa637aedfa003
Chamber6821 <vania.osinin@yandex.ru>
2023-05-21 14:00:21 +0300

    chore: add integration test

---
+++ it/WithStringCase.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Ivan on 10.05.2023.
+//
+
+#include "Reactions/misc/owning/make.h"
+#include "Reactions/response/RsText.h"
+#include "doctest-nolint.h"
+#include <sstream>
+
+TEST_SUITE("Internal UB") {
+    TEST_CASE("make RsText from temporary string") {
+        auto temporaryObject = make<std::string>("Long string");
+        auto text = make<RsText>(*temporaryObject);
+        temporaryObject.reset(); // Call destructor of the temporary object
+
+        std::stringstream buffer;
+        text->print(buffer);
+
+        //REQUIRE(buffer.view() == "Long string");
+    }
+}
---
+++ tests/doctest-nolint.h
@@ -0,0 +1,72 @@
+//
+// Created by Ivan on 23.04.2023.
+//
+
+#pragma once
+
+#ifndef DOCTEST_CONFIG_NO_SHORT_MACRO_NAMES
+#define DOCTEST_CONFIG_NO_SHORT_MACRO_NAMES
+#endif
+
+// NOLINTBEGIN
+#include "doctest/doctest.h"
+// NOLINTEND
+
+#define TEST_CASE              DOCTEST_TEST_CASE              // NOLINT
+#define TEST_CASE_CLASS        DOCTEST_TEST_CASE_CLASS        // NOLINT
+#define TEST_CASE_FIXTURE      DOCTEST_TEST_CASE_FIXTURE      // NOLINT
+#define SUBCASE                DOCTEST_SUBCASE                // NOLINT
+#define TEST_SUITE             DOCTEST_TEST_SUITE             // NOLINT
+#define TEST_SUITE_BEGIN       DOCTEST_TEST_SUITE_BEGIN       // NOLINT
+#define TEST_SUITE_END         DOCTEST_TEST_SUITE_END         // NOLINT
+#define WARN                   DOCTEST_WARN                   // NOLINT
+#define WARN_FALSE             DOCTEST_WARN_FALSE             // NOLINT
+#define WARN_THROWS            DOCTEST_WARN_THROWS            // NOLINT
+#define WARN_THROWS_AS         DOCTEST_WARN_THROWS_AS         // NOLINT
+#define WARN_THROWS_WITH       DOCTEST_WARN_THROWS_WITH       // NOLINT
+#define WARN_THROWS_WITH_AS    DOCTEST_WARN_THROWS_WITH_AS    // NOLINT
+#define WARN_NOTHROW           DOCTEST_WARN_NOTHROW           // NOLINT
+#define CHECK                  DOCTEST_CHECK                  // NOLINT
+#define CHECK_FALSE            DOCTEST_CHECK_FALSE            // NOLINT
+#define CHECK_THROWS           DOCTEST_CHECK_THROWS           // NOLINT
+#define CHECK_THROWS_AS        DOCTEST_CHECK_THROWS_AS        // NOLINT
+#define CHECK_THROWS_WITH      DOCTEST_CHECK_THROWS_WITH      // NOLINT
+#define CHECK_THROWS_WITH_AS   DOCTEST_CHECK_THROWS_WITH_AS   // NOLINT
+#define CHECK_NOTHROW          DOCTEST_CHECK_NOTHROW          // NOLINT
+#define REQUIRE                DOCTEST_REQUIRE                // NOLINT
+#define REQUIRE_FALSE          DOCTEST_REQUIRE_FALSE          // NOLINT
+#define REQUIRE_THROWS         DOCTEST_REQUIRE_THROWS         // NOLINT
+#define REQUIRE_THROWS_AS      DOCTEST_REQUIRE_THROWS_AS      // NOLINT
+#define REQUIRE_THROWS_WITH_AS DOCTEST_REQUIRE_THROWS_WITH_AS // NOLINT
+#define REQUIRE_NOTHROW        DOCTEST_REQUIRE_NOTHROW        // NOLINT
+#define SCENARIO               DOCTEST_SCENARIO               // NOLINT
+#define GIVEN                  DOCTEST_GIVEN                  // NOLINT
+#define WHEN                   DOCTEST_WHEN                   // NOLINT
+#define AND_WHEN               DOCTEST_AND_WHEN               // NOLINT
+#define THEN                   DOCTEST_THEN                   // NOLINT
+#define AND_THEN               DOCTEST_AND_THEN               // NOLINT
+#define WARN_EQ                DOCTEST_WARN_EQ                // NOLINT
+#define CHECK_EQ               DOCTEST_CHECK_EQ               // NOLINT
+#define REQUIRE_EQ             DOCTEST_REQUIRE_EQ             // NOLINT
+#define WARN_NE                DOCTEST_WARN_NE                // NOLINT
+#define CHECK_NE               DOCTEST_CHECK_NE               // NOLINT
+#define REQUIRE_NE             DOCTEST_REQUIRE_NE             // NOLINT
+#define WARN_GT                DOCTEST_WARN_GT                // NOLINT
+#define CHECK_GT               DOCTEST_CHECK_GT               // NOLINT
+#define REQUIRE_GT             DOCTEST_REQUIRE_GT             // NOLINT
+#define WARN_LT                DOCTEST_WARN_LT                // NOLINT
+#define CHECK_LT               DOCTEST_CHECK_LT               // NOLINT
+#define REQUIRE_LT             DOCTEST_REQUIRE_LT             // NOLINT
+#define WARN_GE                DOCTEST_WARN_GE                // NOLINT
+#define CHECK_GE               DOCTEST_CHECK_GE               // NOLINT
+#define REQUIRE_GE             DOCTEST_REQUIRE_GE             // NOLINT
+#define WARN_LE                DOCTEST_WARN_LE                // NOLINT
+#define CHECK_LE               DOCTEST_CHECK_LE               // NOLINT
+#define REQUIRE_LE             DOCTEST_REQUIRE_LE             // NOLINT
+#define WARN_UNARY             DOCTEST_WARN_UNARY             // NOLINT
+#define CHECK_UNARY            DOCTEST_CHECK_UNARY            // NOLINT
+#define REQUIRE_UNARY          DOCTEST_REQUIRE_UNARY          // NOLINT
+#define WARN_UNARY_FALSE       DOCTEST_WARN_UNARY_FALSE       // NOLINT
+#define CHECK_UNARY_FALSE      DOCTEST_CHECK_UNARY_FALSE      // NOLINT
+#define REQUIRE_UNARY_FALSE    DOCTEST_REQUIRE_UNARY_FALSE    // NOLINT
+#define CAPTURE                DOCTEST_CAPTURE                // NOLINT
